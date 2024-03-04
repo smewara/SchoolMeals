83ed25be1151c44d47041e74d55912b4f8ac277f
Fabian Ceccato <fabian.ceccato@senseon.io>
2024-03-04 20:38:13 +0000

    sentimentAnalysis

--- schoolmeals.py
+++ schoolmeals.py
@@ -2,25 +2,29 @@ import nltk
 import pandas as pd
 from nltk.sentiment import SentimentIntensityAnalyzer
 
-school_xsl = 'SurveyData/School_1a_2021.xlsx'
-df = pd.read_excel(school_xsl)
+def sentimentAnalysis(df, col_name):
 
-meat_free_good_idea_q = df['Q36_4_TEXT']
+    sia = SentimentIntensityAnalyzer()
 
-nltk.download('vader_lexicon')
+    sentiments = []
 
-sia = SentimentIntensityAnalyzer()
+    for w in df[col_name].values:
+        sentiments.append(sia.polarity_scores(str(w)))
+    
+    df[col_name+'_SENTIMENT'] = sentiments
 
-sentiments_meat_free_idea = []
+    print(df['Q36_4_TEXT_SENTIMENT'])
 
-for w in meat_free_good_idea_q.values:
-    sentiments_meat_free_idea.append(sia.polarity_scores(str(w)))
+    return df
 
-print(sia.polarity_scores('That feels like we are imposing a choice on children. There are plenty of fish possibilities to replace meat and also quorn  etc'))
+def main():
+    school_xsl = 'SurveyData/School_1a_2021.xlsx'
+    df = pd.read_excel(school_xsl)
 
+    nltk.download('vader_lexicon')
 
-df.insert(df.columns.get_loc('Q36_4_TEXT') + 1, 'Q36_4_TEXT_SENTIMENT', sentiments_meat_free_idea)
+    sentimentAnalysis(df, 'Q36_4_TEXT')
 
-print(df['Q36_4_TEXT_SENTIMENT'])
+    df.to_excel(school_xsl, sheet_name='Sheet1', index=False)
 
-df.to_excel(school_xsl, sheet_name='Sheet2', index=False)
+main()
